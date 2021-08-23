6d1e1e0c22970fecd91c29e6f30e135a062fc64f
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-08-23 13:03:43 +0545

    implemented /treasury

--- v1/statistics/urls.py
+++ v1/statistics/urls.py
@@ -2,7 +2,9 @@ from rest_framework.routers import SimpleRouter
 
 from .views.transaction import TransactionListView
 from .views.government import GovernmentViewSet
+from .views.treasury import TreasuryViewSet
 
 router = SimpleRouter(trailing_slash=False)
 router.register('transaction', TransactionListView)
 router.register('government', GovernmentViewSet)
+router.register('treasury', TreasuryViewSet)
