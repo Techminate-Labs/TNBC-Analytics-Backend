fc9bb8117e3ccdb23c8b41322b92ab1ca2140c56
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-09-07 07:57:30 +0545

    implemented homepage chart

--- v1/statistics/views/charts.py
+++ v1/statistics/views/charts.py
@@ -70,5 +70,31 @@ class GovernmentChart(APIView):
         return Response(data, status=status.HTTP_200_OK)
 
 
-class HomepageChart():
-    pass
+class HomepageChart(APIView):
+    
+    def get(self, request):
+
+        days = request.query_params.get("days", 7)
+
+        try:
+            days = int(days)
+        except ValueError:
+            error = {"error": "Invalid day format!!"}
+            raise serializers.ValidationError(error)
+
+        transactions = Transaction.objects.filter(txs_sent_at__gt=timezone.now() - timedelta(days=days))
+
+        temp = []
+        data = []
+
+        for txs in transactions:
+            temp.append(txs.amount)
+            temp.append(txs.txs_sent_at)
+            data.append(temp)
+            temp = []
+
+        data = {
+            "data": data
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
