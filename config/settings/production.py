748d9c15037c96ea3d3f642a016844698bf2d9d8
SazidAhmed <sazidahmed.official@gmail.com>
2021-10-02 06:15:34 +0600

    getting ready for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,1 +1,12 @@
 from .base import *  # noqa: F401
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+DEBUG = False
+
+ALLOWED_HOSTS = [*]
